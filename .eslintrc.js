002f48e4ca892f55cb73827178afed10d757da79
Katya Kinyapina <kinyapina.katya28@gmail.com>
2024-06-12 23:21:53 +0700

    install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+const module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {}
+}
